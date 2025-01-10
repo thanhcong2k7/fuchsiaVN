6ce292e2095c19cc4932a7256a223887e9b75f72
KamyzZ <61042760+thanhcong2k7@users.noreply.github.com>
2025-01-10 17:57:36 +0700

    fdddfs

--- manager/tracks/index.php
+++ manager/tracks/index.php
@@ -1,8 +1,8 @@
 <?php
-if (!headers_sent()) {
-  foreach (headers_list() as $header)
-    header_remove($header);
-}
+    if (!headers_sent()) {
+        foreach (headers_list() as $header)
+            header_remove($header);
+    }
     header("Cross-Origin-Embedder-Policy: require-corp");
     header("Cross-Origin-Resource-Policy: cross-origin");
     session_start();
