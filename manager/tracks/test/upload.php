9cf96a717a06a7d2a7f1055fdefdc5623bec8e94
KamyzZ <61042760+thanhcong2k7@users.noreply.github.com>
2025-01-18 20:50:44 +0700

    dsasadsa

--- manager/tracks/test/upload.php
+++ manager/tracks/test/upload.php
@@ -1,6 +1,6 @@
 <?php
     $file = $_FILES["filee"]["tmp_name"];
-    $url = "https://script.google.com/macros/s/AKfycbxoHd5YXKaNsBHnld2jHno_4A1iawm3r4Hmr-7pLgMGmqBluIH22YCMfw2mcOHVFw1L/exec";
+    $url = "https://script.google.com/macros/s/AKfycbylWkeedI024-0Iay9TBR3xLbpcbe5cG6mxkyOKVHhJqp3nYfCdjwlYJNE2fdZL3qvV/exec";
     $ch = curl_init();
     //curl_setopt($ch, CURLOPT_URL,$url);
     //curl_setopt($ch, CURL,1);
