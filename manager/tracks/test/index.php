f51571f6bda13d691c6bed25289d5f6e065bfb6d
KamyzZ <61042760+thanhcong2k7@users.noreply.github.com>
2025-01-25 23:01:58 +0700

    tiep

--- manager/tracks/test/index.php
+++ manager/tracks/test/index.php
@@ -16,7 +16,7 @@
   <div id="message"></div>
 
   <script>
-    var urll = "https://script.google.com/macros/s/AKfycbxp9SAD9h8Jbn-SUsZych1WNyMqvxlFzS2fWf5sD7auI9R2PF6vCTZ-z898h2n8Ii8c/exec";
+    var urll = "https://script.google.com/macros/s/AKfycbxN_iQU0-OYm8SOiH0RI_M7oWHIkMI_ZKoWJcRMH7ayyMzygOQXaNZ8GNu0P2ZEba2X/exec";
     document.getElementById('uploadForm').addEventListener('submit', async function (event) {
       event.preventDefault();
 
