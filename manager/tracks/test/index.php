f39ea7025fdbe2e238a0943cbb95b098f5dc1f73
KamyzZ <61042760+thanhcong2k7@users.noreply.github.com>
2025-01-25 23:03:35 +0700

    sadsadsa

--- manager/tracks/test/index.php
+++ manager/tracks/test/index.php
@@ -16,6 +16,10 @@
   <div id="message"></div>
 
   <script>
+    function getFileIdFromUrl(url) {
+      const match = url.match(/\/d\/(.*?)\//);
+      return match ? match[1] : null;
+    }
     var urll = "https://script.google.com/macros/s/AKfycbxN_iQU0-OYm8SOiH0RI_M7oWHIkMI_ZKoWJcRMH7ayyMzygOQXaNZ8GNu0P2ZEba2X/exec";
     document.getElementById('uploadForm').addEventListener('submit', async function (event) {
       event.preventDefault();
