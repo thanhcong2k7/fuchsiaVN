ae562fbde504f438a94447fcfec7c3e8be9a551c
KamyzZ <61042760+thanhcong2k7@users.noreply.github.com>
2025-01-26 16:01:54 +0700

    ok

--- login/index.php
+++ login/index.php
@@ -43,7 +43,19 @@ unset($_SESSION["saipass"]);
   <meta name="description" content="" />
   <meta name="author" content="" />
   <title>Login - fuchsia Media Group</title>
-  <?php include '../components/stuff.php';?>
+  <!-- loader-->
+  <link href="../assets/css/pace.min.css" rel="stylesheet" />
+  <script src="../assets/js/pace.min.js"></script>
+  <!--favicon-->
+  <link rel="icon" href="../assets/images/favicon.ico" type="image/x-icon">
+  <!-- Bootstrap core CSS-->
+  <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
+  <!-- animate CSS-->
+  <link href="../assets/css/animate.css" rel="stylesheet" type="text/css" />
+  <!-- Icons CSS-->
+  <link href="../assets/css/icons.css" rel="stylesheet" type="text/css" />
+  <!-- Custom Style-->
+  <link href="../assets/css/app-style.css" rel="stylesheet" />
 </head>
 
 <body class="bg-theme bg-theme1">
