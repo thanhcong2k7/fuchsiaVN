a3f27b72bc4ad773ede5d7cf76b0d4b674ef8c8c
KamyzZ <61042760+thanhcong2k7@users.noreply.github.com>
2025-01-26 16:48:34 +0700

    ok

--- login/index.php
+++ login/index.php
@@ -169,6 +169,16 @@ unset($_SESSION["saipass"]);
 
   </div>
   <!--wrapper-->
+  <!-- Bootstrap core JavaScript-->
+  <script src="/assets/js/jquery.min.js"></script>
+  <script src="/assets/js/popper.min.js"></script>
+  <script src="/assets/js/bootstrap.min.js"></script>
+
+  <!-- sidebar-menu js -->
+  <script src="/assets/js/sidebar-menu.js"></script>
+
+  <!-- Custom scripts -->
+  <script src="/assets/js/app-script.js"></script>
 </body>
 
 </html>
