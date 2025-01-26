c682a877feb7472fc09ca18c37ad03a4362016d7
KamyzZ <61042760+thanhcong2k7@users.noreply.github.com>
2025-01-26 16:58:21 +0700

    Block users from accessing resources

---
+++ assets/js/upload-drive.js
@@ -0,0 +1,57 @@
+function getFileIdFromUrl(url) {
+    const match = url.match(/\/d\/(.*?)\//);
+    return match ? match[1] : null;
+  }
+  document.getElementById('uploadForm').addEventListener('submit', async function (event) {
+    event.preventDefault();
+    var urll = "https://script.google.com/macros/s/AKfycbxN_iQU0-OYm8SOiH0RI_M7oWHIkMI_ZKoWJcRMH7ayyMzygOQXaNZ8GNu0P2ZEba2X/exec";
+    var fileInput = document.getElementById("fileInput");
+    if (fileInput.files.length === 0) {
+      document.getElementById("message").innerText = "Vui lòng chọn một file!";
+      return;
+    }
+
+    var file = fileInput.files[0];
+    const GAS_DEPLOY = urll;
+
+    try {
+      const base64String = await encodeFileToBase64(file);
+
+      var formData = new FormData();
+      formData.append("name", file.name);
+      formData.append("type", file.type);
+      formData.append("file", base64String);
+
+      fetch(GAS_DEPLOY, {
+        method: 'POST',
+        body: formData
+      })
+        .then(response => response.json())
+        .then(data => {
+          if (data.status === "success") {
+            let fileID = getFileIdFromUrl(data.url);
+            document.getElementById("message").innerText = fileID;
+            console.log(fileID);
+          } else {
+            document.getElementById("message").innerText = "Lỗi: " + data.message;
+          }
+        })
+        .catch(error => {
+          document.getElementById("message").innerText = "Lỗi kết nối!";
+          console.error(error);
+        });
+
+    } catch (error) {
+      document.getElementById("message").innerText = "Lỗi mã hóa tệp!";
+      console.error(error);
+    }
+  });
+
+  function encodeFileToBase64(file) {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = () => resolve(reader.result.split(',')[1]);
+      reader.onerror = (error) => reject(error);
+      reader.readAsDataURL(file);
+    });
+  }
--- manager/tracks/test/index.php
+++ manager/tracks/test/index.php
@@ -20,7 +20,6 @@
       const match = url.match(/\/d\/(.*?)\//);
       return match ? match[1] : null;
     }
-
     document.getElementById('uploadForm').addEventListener('submit', async function (event) {
       event.preventDefault();
       var urll = "https://script.google.com/macros/s/AKfycbxN_iQU0-OYm8SOiH0RI_M7oWHIkMI_ZKoWJcRMH7ayyMzygOQXaNZ8GNu0P2ZEba2X/exec";
