1c92675a8758a24467a00cf0fa5ea17be152a7d7
KamyzZ <61042760+thanhcong2k7@users.noreply.github.com>
2025-01-29 17:56:33 +0700

    fsdfdsfsdfdsfdsssfdsfds

--- discography/edit.php
+++ discography/edit.php
@@ -55,6 +55,9 @@ if (isset($_GET["new"]) && isset($_SESSION["userwtf"])) {
     <script src="/assets/js/popper.min.js"></script>
     <script src="/assets/js/bootstrap.min.js"></script>
     <script src="/assets/js/select2.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.0/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 </head>
 
 <body class="bg-theme bg-theme1">
