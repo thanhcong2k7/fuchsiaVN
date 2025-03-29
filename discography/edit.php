7f1dbdf5289dedb78c204873fa9c327ea952499c
KamyzZ <61042760+thanhcong2k7@users.noreply.github.com>
2025-03-29 11:57:30 +0700

    test

--- discography/edit.php
+++ discography/edit.php
@@ -571,29 +571,34 @@ $currentYear = date("Y");
                                             </div>
                                             <div class="card-body">
                                                 <div class="row">
+
                                                     <div class="col-12">
                                                         <label for="stores">Stores/Services</label>
-                                                        <div class="input-group select2-bootstrap-append">
-                                                            <select name="stores[]" class="form-control" id="stores"
-                                                                multiple
-                                                                data-placeholder="Choose stores/services to distribute to"
-                                                                style="width:100%;">
-                                                                <?php
-                                                                $availableStores = getStore(); // Assume returns array of store objects (id, name)
-                                                                $selectedStores = $release->stores ?? []; // Assume $release->stores is an array of selected store IDs
-                                                                if ($availableStores) {
-                                                                    foreach ($availableStores as $s) {
-                                                                        $isSelected = in_array($s->id, $selectedStores) ? 'selected' : '';
-                                                                        echo '<option value="' . $s->id . '" ' . $isSelected . '>' . htmlspecialchars($s->name, ENT_QUOTES, 'UTF-8') . '</option>';
+                                                        <div class="row align-items-center no-gutters">
+                                                            <div class="col"> <select name="stores[]"
+                                                                    class="form-control" id="stores" multiple
+                                                                    data-placeholder="Choose stores/services to distribute to"
+                                                                    style="width:100%;">
+                                                                    <?php
+                                                                    $availableStores = getStore(); // Assume returns array of store objects (id, name)
+                                                                    $selectedStores = $release->stores ?? []; // Assume $release->stores is an array of selected store IDs
+                                                                    if ($availableStores) {
+                                                                        foreach ($availableStores as $s) {
+                                                                            $isSelected = in_array($s->id, $selectedStores) ? 'selected' : '';
+                                                                            echo '<option value="' . $s->id . '" ' . $isSelected . '>' . htmlspecialchars($s->name, ENT_QUOTES, 'UTF-8') . '</option>';
+                                                                        }
                                                                     }
-                                                                }
-                                                                ?>
-                                                            </select>
-                                                            <div class="input-group-append">
-                                                                <span class="input-group-text">
-                                                                    <input type="checkbox" id="toggleSelectAllStores"
+                                                                    ?>
+                                                                </select>
+                                                            </div>
+                                                            <div class="col-auto pl-2">
+                                                                <div class="form-check"> <input class="form-check-input"
+                                                                        type="checkbox" id="toggleSelectAllStores"
                                                                         title="Select/Deselect All">
-                                                                </span>
+                                                                    <label class="form-check-label small"
+                                                                        for="toggleSelectAllStores"> All
+                                                                    </label>
+                                                                </div>
                                                             </div>
                                                         </div>
                                                     </div>
