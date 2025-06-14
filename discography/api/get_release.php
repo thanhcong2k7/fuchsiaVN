87734c072655ae2cfefed1e34872230cb401250c
KamyzZ <61042760+thanhcong2k7@users.noreply.github.com>
2025-06-14 23:21:55 +0700

    releaselength

--- discography/api/get_release.php
+++ discography/api/get_release.php
@@ -19,7 +19,7 @@ try {
     // Get release data
     $release = getRelease($_SESSION["userwtf"], 0, $releaseId);
     echo $_SESSION['userwtf']."\n";
-    //echo $release."\n";
+    echo $release->length."\n";
     if (!$release) {
         throw new Exception('Release not found');
     }
