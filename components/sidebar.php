8693a390bf6db6ba0125f248e967317b69cfeb40
KamyzZ <61042760+thanhcong2k7@users.noreply.github.com>
2025-06-14 07:57:11 +0700

    wtf

--- components/sidebar.php
+++ components/sidebar.php
@@ -111,8 +111,8 @@
         </div>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal"
-          aria-label="Close payment pop-up" id="closePayment">Close</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal" aria-label="Close payment pop-up"
+          id="closePayment">Close</button>
       </div>
     </div>
   </div>
@@ -120,14 +120,14 @@
 <script src="/components/payment.js"></script>
 <!--Start of Tawk.to Script-->
 <script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/5c17215c7a79fc1bddf13285/default';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
+  var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+  (function () {
+    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = 'https://embed.tawk.to/5c17215c7a79fc1bddf13285/default';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
+  })();
 </script>
 <!--End of Tawk.to Script-->
